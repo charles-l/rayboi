ed5506c42f9f51563d4068f2f8f42f659b604a39
nc <charles.saternos@gmail.com>
2021-01-24 11:15:40 -0500

    add shadows

--- rayboi2.py
+++ rayboi2.py
@@ -16,8 +16,8 @@ class Sphere:
         :returns: boolean array of hits, array of distances (floats)
         '''
         L = self.center - orig
-        tca = direction @ L
-        d2 = L @ L - tca**2
+        tca = v3_dots(L.reshape((-1, 3)), direction.reshape((-1, 3)))
+        d2 = v3_dots(L, L) - tca**2
         # early check would normally check d2 > self.radius**2
         # FIXME: I don't think nans should be floating around here...
         thc = np.sqrt(self.radius**2 - d2)
@@ -25,8 +25,8 @@ class Sphere:
         t1 = tca + thc
         t0[t0 < 0] = t1[t0 < 0]
         return (np.where(d2 > self.radius**2, False,
-                        np.where(t0 < 0, False, True)),
-                t0)
+                        np.where(t0 < 0, False, True)).ravel(),
+                t0.ravel())
 
 
 def normalize(v):
@@ -53,7 +53,9 @@ def v3_dots(a, b):
     perform a dot product across the vector3s in a and b.
     e.g. v3_dots([v1, v2, v3], [v4, v5, v6]) => [v1 @ v4, v2 @ v5, v3 @ v6]
     '''
-    assert a.shape == b.shape and a.shape[1] == b.shape[1] == 3
+    assert len(a.shape) == len(b.shape) == 2
+    assert a.shape[0] == b.shape[0] or (1 in (a.shape[0], b.shape[0])), f"can't broadcast a and b: {a.shape=}, {b.shape=}"
+    assert a.shape[1] == b.shape[1] == 3, (a.shape, b.shape)
     return np.sum(a * b, axis=1)
 
 
@@ -86,15 +88,14 @@ def render():
 
     X, Y = np.meshgrid(np.arange(width), np.arange(height))
     xs =  (2*(X+0.5) / width - 1) * np.tan(fov/2) * width/height
-    print(xs.min(), xs.max())
     ys =  (2*(Y+0.5) / height - 1) * np.tan(fov/2)
     dirs = normalize(np.dstack((xs, ys, -1 * np.ones((height, width)))).reshape((-1, 3)))
 
-    orig = np.array([0, 0, 0])
+    orig = np.array([[0, 0, 0]])
     dists, sphere_map = scene_intersect(orig, dirs, spheres)
-    hits = orig + dists.reshape((-1, 1)) * dirs
+    points = orig + dists.reshape((-1, 1)) * dirs
 
-    N = normalize(hits - sphere_centers[sphere_map])
+    N = normalize(points - sphere_centers[sphere_map])
 
     light_positions = np.array([(-20, 20, 20), (30, 50, -25), (30, 20, 30)])
     light_intensities = np.array([1.5, 1.8, 1.7])
@@ -103,14 +104,24 @@ def render():
     specular_intensity = np.zeros((width*height,), dtype=np.float64)
 
     for i in range(len(light_positions)):
-        light_dir = normalize(light_positions[i] - hits)
+        light_dir = normalize(light_positions[i] - points)
+        light_distance = np.linalg.norm(light_positions[i] - points, axis=1)
+
+        shadow_orig = np.where((v3_dots(light_dir.reshape((-1, 3)), N) < 0).reshape((-1, 1)),
+                               points - N * 1e-3,
+                               points + N * 1e-3)
+
+        shadow_dists, shadow_map = scene_intersect(shadow_orig, light_dir, spheres)
+        shadow_points = orig + shadow_dists.reshape((-1, 1)) * light_dir
+        shadow_mask = shadow_map != -1 # & (np.linalg.norm(shadow_points - shadow_orig, axis=1) < light_distance)
+
         d = v3_dots(light_dir, N)
-        diffuse_intensity += light_intensities[i] * np.where(d < 0, 0, d)
+        diffuse_intensity += light_intensities[i] * np.where((d < 0) | shadow_mask, 0, d)
 
         reflect = light_dir - 2 * N * v3_dots(light_dir, N).reshape((-1, 1))
         reflect_dot_dir = v3_dots(reflect, dirs)
         spec_exponents = sphere_materials['specular_exponent'][np.where(sphere_map != -1, sphere_map, 0)]
-        specular_intensity += light_intensities[i] * np.power(np.where(reflect_dot_dir < 0, 0, reflect_dot_dir), spec_exponents)
+        specular_intensity += light_intensities[i] * np.power(np.where((reflect_dot_dir < 0) | shadow_mask, 0, reflect_dot_dir), spec_exponents)
 
     # clear to bg color
     framebuffer[:,:] = (0.2, 0.7, 0.8)
